1e7558f26a5f4d46f76dfed4d5555ebf67a5ec50
NeisserMS <rko_619_fbi@hotmail.com>
2023-07-10 14:40:50 -0500

    1ra parte estable

--- Biblioteca/Biblioteca/Entidades/Autor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Biblioteca.Entidades
-{
-    public class Autor
-    {
-        public int id { get; set; }
-        public string nombre { get; set; } 
-    }
-}
---
+++ WebAPIAutores/WebAPIAutores/Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    /* Heredamos de Controller Base y tbn importamos */
+    [ApiController] /* Con este atributo indicamos que la clase es un controlador de API y habilita su comportamiento especifico. */
+    [Route("api/autores")] /* Este atributo establece la ruta base para las acciones dentro del controlador. Los [] en autores indica que es una variable que se va a reemplazar con un valor real.*/
+    public class AutoresController: ControllerBase /* Hereda de ControllBase para poder usar sus funcionalidades y metodos GET. */
+    {
+        [HttpGet] /* Se define el método */
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { id = 1, nombre = "Jair" },
+                new Autor() { id = 2, nombre = "Neisser" },
+            };
+        }
+    }
+}
